eab751498217ccb4c8b10cf09f2f6b27a5183026
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-23 14:14:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,17 +15,17 @@ st.subheader("Explore the collection of apps built by Harshal Panchal")
 # List of apps with their descriptions and links
 apps = [
     {
-        "name": "VizFlex",
+        "name": "📊 VizFlex",
         "description": "An intuitive data visualization tool that allows users to upload CSV files and create various plots.",
         "link": "https://vizflex.streamlit.app/"
     },
     {
-        "name": "AnalyzeKit - Because Life's Too Short for Mundane Analysis!",
-        "description": "AnalyseKit is a web application designed to provide automated exploratory data analysis (EDA) for CSV datasets. It allows users to quickly understand their dataset by visualizing key insights and statistical summaries.",
+        "name": "📈 AnalyzeKit - Because Life's Too Short for Mundane Analysis!",
+        "description": "AnalyzeKit is a web application designed to provide automated exploratory data analysis (EDA) for CSV datasets. It allows users to quickly understand their dataset by visualizing key insights and statistical summaries.",
         "link": "https://analyzekit.streamlit.app/"
     },
     {
-        "name": "Dork's Data Digest - Navigating the Literary Universe to Find Your Perfect Match!",
+        "name": "📚 Dork's Data Digest - Navigating the Literary Universe to Find Your Perfect Match!",
         "description": "Dork's Data Digest is your ultimate guide to the world of data science literature. Our platform utilizes advanced algorithms to deliver personalized recommendations, ensuring you discover the most relevant and insightful reads. Say goodbye to aimless browsing and let us streamline your journey to becoming a data science aficionado!",
         "link": "https://dorksdataaadigest.streamlit.app/"
     }
@@ -35,7 +35,7 @@ apps = [
 # Display each app with its description and link
 for app in apps:
     st.markdown(f"### [{app['name']}]({app['link']})")
-    st.markdown(f"*{app['description']}*")
+    st.markdown(f"{app['description']}")
 
 # Footer
 st.markdown(
